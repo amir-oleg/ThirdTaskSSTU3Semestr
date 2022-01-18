4809dee34706ca1f4250e6c79b672787d83f98c7
Ol-Am <oleg_amirov@epam.com>
2022-01-19 02:01:27 +0400

    Add project files.

---
+++ ThirdTask/EditDbForm.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThirdTask
+{
+    public partial class EditDbForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Insert(object sender, EventArgs e)
+        {
+            SqlDataSource1.Insert();
+        }
+
+        protected void Insert2(object sender, EventArgs e)
+        {
+            SqlDataSource2.Insert();
+        }
+    }
+
+}
